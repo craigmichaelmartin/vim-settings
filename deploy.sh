fd1ba90d1313ed967edc195bbbdfa91a7a18927a
Craig Martin <craig@kujoyardwear.com>
2019-03-27 21:09:57 -0400

    maybe fix

--- deploy.sh
+++ deploy.sh
@@ -25,8 +25,11 @@ git submodule update
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
+git submodule init
+git submodule update
+
 cd ~/.vim/bundle && git clone git://github.com/altercation/vim-colors-solarized.git
-mkdir ~/vim/colors && mv ~/.vim/bundle/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/
+mkdir ~/.vim/colors && mv ~/.vim/bundle/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/
 
 mkdir -p ~/.vim/autoload ~/.vim/bundle && curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
